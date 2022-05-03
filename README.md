bc39dee7344793513a2df9c1804bcb98b0c616d3
Qrashi <fritz@vibe.ac>
2022-05-03 15:03:10 +0200

    Update README.md
    Update visibility of some attributes

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # easyjson
 Load and save json files safely and fast.
+
+Installation:
+```shell
+pip install easyjson
+```
+
+Usage:
+```python
+import easyjson
+
+file = easyjson.open('file.json')  # Load file.json
+file.json = {"key": "value"}  # Change values of JSON file.
+file2 = easyjson.open('file.json')
+# file and file2 are the same!
+easyjson.sync()  # Sync all changes to disk
+```
+
+Other operations possible:
+```python
+import easyjson
+
+file = easyjson.open('file.json')  # Load file.json
+file.save()  # Save the current contents
+file.reload()  # Reload the contents from disk
+```
--- easyjson/__init__.py
+++ easyjson/__init__.py
@@ -7,22 +7,22 @@ sync all changes to disk using sync()
 """
 from typing import List
 
-import jsonutils
+import utils as __jsonutils
 
-_file_pool: List[jsonutils.JsonFile] = {}
+__file_pool: List[__jsonutils.JsonFile] = {}
 
 
-def open(filename: str, default: str = "{}") -> jsonutils.JsonFile:
+def open(filename: str, default: str = "{}") -> __jsonutils.JsonFile:
     """
     Open a JsonFile (synchronously)
     :param filename: Path to JSON file on disk
     :param default: Default file contents to save if file is nonexistent
     :return: the corresponding JsonFile
     """
-    filename = jsonutils.abs_filename(filename)
-    if filename not in _file_pool:
-        _file_pool[filename] = jsonutils.JsonFile(filename, default=default)
-    return _file_pool[filename]
+    filename = __jsonutils.abs_filename(filename)
+    if filename not in __file_pool:
+        __file_pool[filename] = __jsonutils.JsonFile(filename, default=default)
+    return __file_pool[filename]
 
 
 def sync():
@@ -30,5 +30,5 @@ def sync():
     Sync changes to the filesystem
     :return:
     """
-    for file in _file_pool:
+    for file in __file_pool:
         file.save()
